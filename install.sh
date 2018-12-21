8b27a1922b67cb037199126e6f8fe7b9fdea7c2b
12ColcloughJ <12colcloughj@lytchett.org.uk>
2018-12-21 19:20:09 +0000

    Script added

---
+++ install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+pacman -S i3-gaps i3status rofi ranger firefox lxappearance adapta-gtk-theme papirus-icon-theme git go python python-pip python2 termite gnome-disk-utility gnome-calculator blueman nitrogen vim htop
